a9cd7cd8695d1f31a702ae602949434ed86e6d50
import-yuefeng <zhuyuefeng0@outlook.com>
2018-12-13 20:34:04 +0800

    Create downloadSongData.py

---
+++ downloadSongData.py
@@ -0,0 +1,27 @@
+import json
+import requests
+import time
+
+albumId = input("[~] Enter your albumId: ")
+
+fp = open("result-json-{}.txt".format(albumId), "r", encoding="UTF-8")
+content = eval(fp.read())
+
+HEADERS = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) \
+        Gecko/20100101 Firefox/58.0',
+    'Referer':'https://www.ximalaya.com'    
+}
+
+for item in content:
+    try:
+        with open('./%s.m4a' % str(item["musicIndex"]), 'wb') as f:
+            response = requests.get(url=item["musicUrl"], headers=HEADERS)
+            f.write(response.content)
+            time.sleep(1)
+    except:
+        print("[-] 出错!, 最后一个下载的内容为%s")
+
+    else:
+        print("\r已下载-> ".format(item["musicIndex"])+str(item["musicIndex"]), end="")
+
